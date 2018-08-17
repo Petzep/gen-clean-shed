b21df87ae7cdd84d98925fb87674cf37f3145d4b
b10n <b10n@dittes.nl>
2018-08-17 15:20:20 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,64 @@
+# gen-clean-shed
+
+This is a simple script I wrote to generate a cleaning schedule. It may be useful to you when you live in a shared household.
+
+At the moment only one pattern, i.e. division of chores per week, is supported. More specifically this means that all generated schedules contain 4 different chores, 2 of which return every week while the other 2 alternate between the weeks. As you probably want something different, you'll have to modify the script.
+
+## Usage
+
+First, set the variables at the top of the `gen_clean_shed.py`. Example values are provided in the source.
+
+| variable          | meaning |
+| ----------------- | ------- |
+| `year`            | year of the schedule |
+| `switch_week`     | week to switch turns |
+| `output_format`   | can be either `csv` or `latex` |
+| `filename`        | filename of output |
+
+
+The following variables are only used for LaTeX output.
+
+| variable          | meaning |
+| ----------------- | ------- |
+| `language`        | can be either 'en' or 'nl' |
+| `holiday_weeks`   | weeks to highlight (optional) |
+| `filename_header` | filename of the header |
+| `filename_footer` | filename of the footer |
+
+Now simply run `python3 gen_clean_shed.py` and it will produce an output file.
+
+## Output format
+
+Two output formats are supported: CSV and LaTeX.
+
+### CSV
+
+This writes every week to a line in a comma-separated value file. It is the format that is expected by [cleaning-bot](https://github.com/flatraad/cleaning-bot).
+The output will look something like this:
+```
+1,D,A,,B
+2,F,,G,H
+3,C,D,,A
+...
+```
+
+Empty fields signify that the chore is not available in that week.
+
+### LaTeX
+
+In this mode the script will generate a LaTeX source file (a `.tex` file). To this end it concatenates the file pointed to by `filename_header` with the content and lastly with the file `filename_footer`.
+
+The result will be a table in with descriptions and dates. The rows corresponding to the weeknumbers in `holiday_weeks` will be highlighted. The descriptions are defined in the files `strings-en.dict` and `strings-nl.dict`, depending on the `language` setting.
+
+The easiest way to get an idea of what this mode entails, is to simply try it.
+Since the output file is a source file to LaTeX, it needs to be compiled with `pdflatex` or another engine to produce a PDF.
+
+## Possible improvements
+
+* use command-line arguments instead of hardcoding settings
+* specify range of week numbers
+* variable amount of rooms
+* allow different patterns
+* LaTeX integration (e.g. autocompilation)
+
+Note: I am no longer using this script, so don't expect any of this being implemented anytime soon.
---
+++ gen_clean_shed.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Script to generate a cleaning schedule in LaTeX or plain csv.
+'''
+
+# General settings:
+year = 2018
+switch_week = 28 # first week of the summer holidays
+output_format = 'csv'
+filename = 'schedule.csv'
+
+# LaTeX-specific settings:
+language = 'en'
+holiday_weeks = [1,7,28,29,30,31,32,33,34,35,52]
+filename_header = 'header.tex'
+filename_footer = 'footer.tex'
+
+from datetime import date, timedelta
+import locale
+import ast
+
+def get_week_days(week):
+    d = date(year,1,1)
+    if(d.weekday()>3):
+        d = d + timedelta(7-d.weekday())
+    else:
+        d = d - timedelta(d.weekday())
+    dlt = timedelta(days = (week-1)*7)
+    return d + dlt,  d + dlt + timedelta(days=6)
+
+'''
+Writes the contents of inf to of, substituting tokens that appear in strings.
+'''
+def insert_file(inf, of, strings):
+    file = open(inf, 'r')
+    for line in file:
+        for token, string in strings.items():
+            line = line.replace(token, string)
+        of.write(line)
+    return
+
+'''
+Writes the contents line to of.
+The necessary conversions are made in case the output format is latex.
+For csv, no changes are necessary.
+'''
+def output_line(of, line):
+    if output_format == 'csv':
+        # the line is already in the correct format:
+        # weekno., chore1, chore2, chore3, chore4
+        # where choreX denotes the responsible for the chore, if any
+        of.write(line + '\n')
+
+    elif output_format == 'latex':
+        # convert the line stepwise to a LaTeX tabular row
+        # example:
+        # 1 & jan 2 & jan 8 & E \check & F \check &  & G \check \\
+
+        i = int(line.split(',', 1)[0]) # the weekno.
+
+        # highlight the row if it is a holiday week
+        if i in holiday_weeks:
+            # use different tints for even and odd weeks
+            # note: the package xcolor does not seem to support
+            # multiple alternating colorschemes in one table
+            if i % 2 == 0:
+                of.write('\\rowcolor{\evencolor}\n')
+            if i % 2 == 1:
+                of.write('\\rowcolor{\oddcolor}\n')
+
+        # calculate the dates from the week number
+        of.write(str(i) + ' & ' +  get_week_days(i)[0].strftime('%b'))
+        of.write(' ' + get_week_days(i)[0].strftime('%d').lstrip('0'))
+        of.write(' & ' + get_week_days(i)[1].strftime('%b'))
+        of.write(' ' + get_week_days(i)[1].strftime('%d').lstrip('0'))
+
+        # the remainder can simply be substituted
+        rest = line.split(',', 1)[-1]
+        rest = rest.replace(',,', ', & ')
+        rest = rest.replace(',', ' \\check & ')
+        of.write(' & ' + rest + ' \\check ')
+        if i == switch_week:
+            of.write('\\tikzmark{sidenote} ' )
+        of.write('\\\\\n')
+    return
+
+'''
+Determines which room is doing what chore for all weeks of the year.
+'''
+def generate_schedule(of):
+    rooms = [['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H']] # the suffixes
+    turn = [3, 1] # 0,3 means A,H etc.
+
+    # i is the weeknumber
+    for i in range(1,53):
+        line = '' 
+
+        # at the beginning of the summer holidays,
+        # the alternation of sides is swapped
+        if i < switch_week:
+            side = (i-1) % 2
+        else:
+            side = i % 2
+
+        line += str(i) # weeknumber
+
+        # determine three chores
+        for j in range(3):
+            if i % 2 == 0 and j == 1:
+                # leave chore 2 blank
+                line += ','
+            line += ',' + rooms[side][turn[side]]
+            if i % 2 == 1 and j == 1:
+                # leave chore 3 blank
+                line += ','
+            turn[side] += 1
+            if turn[side] == 4:
+                turn[side] = 0
+
+        output_line(of, line)
+    return
+
+'''
+The main function.
+'''
+def main():
+    of = open(filename, 'w')
+
+    if output_format == 'csv':
+        generate_schedule(of)
+
+    elif output_format == 'latex':
+        # set locale for the name of the month
+        if language == 'nl':
+            locale.setlocale(locale.LC_TIME, 'nl_NL.utf8')
+        else:
+            locale.setlocale(locale.LC_TIME, 'en_US.utf8')
+
+        # load translation strings
+        with open('strings-' + language + '.dict','r') as inf:
+            strings = ast.literal_eval(inf.read())
+
+        # insert the header
+        insert_file(filename_header, of, strings)
+
+        # insert the table body
+        generate_schedule(of)
+
+        # insert the footer
+        insert_file(filename_footer, of, strings)
+
+main()
